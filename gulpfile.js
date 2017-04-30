91c4e71f1253b78909d82dee17dfabc09becdc2c
tewkanz <joshuacadungog@gmail.com>
2017-04-29 20:28:15 -0500

    Get build pipeline figured out
    It's working, but I need a generic way to browserify +babel transpile
    stuff.

---
+++ gulpfile.js
@@ -0,0 +1,27 @@
+var fs = require('fs');
+var gulp = require('gulp');
+var babel = require('gulp-babel');
+var inject = require('gulp-inject');
+var browserify = require('gulp-browserify-thin');
+
+gulp.task('build', [ 'browserify'], () => {
+    var injectTarget = gulp.src('./core/index.html');
+    // eventually we'll need to also pipe in css
+    return injectTarget.pipe(inject(gulp.src('./bin/*.js'))).pipe(gulp.dest('bin'));
+});
+gulp.task('browserify', () => {
+    var babelrc = fs.readFileSync("./.babelrc", { encoding: 'UTF-8'});
+    var babelOptions = JSON.parse(babelrc);
+    var b = browserify({ baseDir: __dirname })
+        .add('./core/views/shell.jsx')
+        .transform("babelify", babelOptions)
+    var stream = b.bundle("browserifiedFile.js");
+    return browserifiedFile = stream.on('error', (err) => {
+        console.error(err.toString());
+        process.exit(1);
+    }).pipe(gulp.dest('bin'));
+})
+gulp.task('run', ['build'], () => {
+    require('./bin/www');
+});
+gulp.task('default', ['build', 'run'], () => {})
---
+++ haltNodeProcess.ps1
@@ -0,0 +1,3 @@
+﻿$processes = & netstat -ano | FindStr "LISTENING" | FindStr "3000"
+$processes = -split $processes[0]
+kill $processes[4]
